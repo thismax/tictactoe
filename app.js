3c3d3bc1ea1a55f92c32bd022af2f1e59bf8d981
thismax <max.g.jacobs@gmail.com>
2017-11-07 14:20:13 -0800

    stubbed out board

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 var prompt = require('prompt');
 
+var Game = function() {
+  this.board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+  this.player = 'O';
+  this.moves = 0;
+}
+
+Game.prototype.move = function () {
+  
+}
