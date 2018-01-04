247ed92496c7736392929bf9fb7cd98584f70c1c
thismax <max.g.jacobs@gmail.com>
2018-01-03 16:07:06 -0800

    minor tweaks

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ var prompt = require('readline-sync');
 
 class Game {
   constructor() {
-    this.board = [
-      [1, 2, 3],
-      [4, 5, 6],
-      [7, 8, 9]
-    ];
+    this.board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
     this.player = 'X';
     this.moves = 0;
   }
@@ -31,11 +27,11 @@ class Game {
   makeMove(move) {
     var row = Math.floor((move - 1) / 3);
     var col = (move - 1) % 3;
-    return {row, col};
+    return { row, col };
   }
 
   isPositionOccupied(move) {
-    var {row, col} = this.makeMove(move);
+    var { row, col } = this.makeMove(move);
     return typeof this.board[row][col] === 'string';
   }
 
@@ -66,23 +62,26 @@ class Game {
   isColWinner(col) {
     return this.areAllEqual(this.board[0][col], this.board[1][col], this.board[2][col]);
   }
-  
+
   isDiagonalWinner() {
-    return this.areAllEqual(this.board[0][0], this.board[1][1], this.board[2][2]) || this.areAllEqual(this.board[0][2], this.board[1][1], this.board[2][0]);
+    return (
+      this.areAllEqual(this.board[0][0], this.board[1][1], this.board[2][2]) ||
+      this.areAllEqual(this.board[0][2], this.board[1][1], this.board[2][0])
+    );
   }
-  
+
   isWinner(row, col) {
     return this.isRowWinner(row) || this.isColWinner(col) || this.isDiagonalWinner();
   }
-  
-  isDraw () {
+
+  isDraw() {
     return this.moves === 9;
   }
-  
+
   printWinner() {
     console.log(`Congratulations, ${this.player}, you've won!`);
   }
-  
+
   getMove() {
     var move;
     var move = prompt.question(`Player ${this.player}, it's your turn! Please choose a move (1-9): `);
@@ -92,23 +91,22 @@ class Game {
       return this.makeMove(move);
     }
   }
-  
+
   play() {
     this.printBoard();
-    var {row, col} = this.getMove();
+    var { row, col } = this.getMove();
     this.move(row, col);
     if (this.isWinner(row, col)) {
+      this.printBoard();
       this.printWinner();
     } else if (this.isDraw()) {
-      console.log( 'It\'s a draw' );
+      console.log("It's a draw");
     } else {
       this.nextPlayer();
       this.play();
     }
   }
-
 }
-
 
 var game = new Game();
 game.play();
